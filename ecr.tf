89705b7e1d956eaa19837d9be30952416e123932
Malik Ivory <300kleek@gmail.com>
2026-09-22 04:10:09 -0400

    Provision ECR repository with Terraform

---
+++ ecr.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "app" {
+  name                 = "app"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Project   = "AWS Blue Green Container Delivery Platform"
+    ManagedBy = "terraform"
+  }
+}
